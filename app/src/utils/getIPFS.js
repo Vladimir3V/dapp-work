57fe045da006b8f4969d0482ccbae2b61ff0cf91
drag0no <krotpv@gmail.com>
2018-08-24 22:59:20 +0300

    Changed IPFS_HOST from localhost to Infura

--- app/src/utils/getIPFS.js
+++ app/src/utils/getIPFS.js
@@ -1,10 +1,11 @@
-const IPFS_HOST = 'localhost'
+// const IPFS_HOST = 'localhost'
+const IPFS_HOST = 'ipfs.infura.io'
 const IPFS_PORT = 5001
 
 const IpfsApi = require('ipfs-api');
 const ipfs = IpfsApi({
     host: IPFS_HOST,
     port: IPFS_PORT,
-    protocol: 'http'
+    protocol: 'https'
 })
 export default ipfs
