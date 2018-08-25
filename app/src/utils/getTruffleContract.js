1f2d780c6e49306e7e99e1db19c30a669aad8c56
drag0no <krotpv@gmail.com>
2018-08-25 12:09:58 +0300

    Added Rinkeby Test Net support for the app with the predefined address

--- app/src/utils/getTruffleContract.js
+++ app/src/utils/getTruffleContract.js
@@ -1,3 +1,4 @@
+const CONTRACT_ADDRESS = "0x2a1c7ac065eebc36de37b9836580f6d18421899d";
 const Web3 = require('web3');
 const contract = require('truffle-contract');
 const contractData = require('../../../build/contracts/DappWork.json');
@@ -6,10 +7,14 @@ let getContract = new Promise(function (resolve, reject) {
     let web3 = new Web3(window.web3.currentProvider)
     const DappWork = contract(contractData);
     DappWork.setProvider(web3.currentProvider);
-    DappWork.deployed().then(async instance => {
-        resolve(instance);
+    DappWork.at(CONTRACT_ADDRESS).then(async instance => {
+        resolve(instance)
     }).catch(error => {
-        reject(error);
-    });
+        DappWork.deployed().then(async instance => {
+            resolve(instance)
+        }).catch(error => {
+            reject(error)
+        })
+    })
 });
 export default getContract
